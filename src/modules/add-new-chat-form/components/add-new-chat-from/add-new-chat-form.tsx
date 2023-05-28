5c8b6a781fdac927cad1483d36ef28474de9001f
darker <darkerros@yandex.ru>
2023-05-28 19:28:17 +0300

    fix: refactor new chat form

--- src/modules/add-new-chat-form/components/add-new-chat-from/add-new-chat-form.tsx
+++ src/modules/add-new-chat-form/components/add-new-chat-from/add-new-chat-form.tsx
@@ -1,5 +1,5 @@
 import styles from './add-new-caht-form.module.scss';
-import {ChangeEventHandler, FC, FormEventHandler, KeyboardEventHandler, useState} from "react";
+import {ChangeEventHandler, FC, FormEventHandler, useState} from "react";
 import {Input} from "../../../../ui/input/input";
 import {SubmitButton} from "../submit-button/submit-button";
 
@@ -12,19 +12,13 @@ export const AddNewChatForm:FC<AddNewChatFormProps> = ({error, onSubmit}) => {
     const [phone, setPhone] = useState<string>('')
 
     const handlePhoneChange:ChangeEventHandler<HTMLInputElement> = (evt) => {
-        const inputValue = evt.currentTarget.value
-        setPhone(inputValue)
-    }
-    const handlePhoneInputKeyPress:KeyboardEventHandler<HTMLInputElement> = (evt) => {
-        const parseNumberValue = Number.parseInt(evt.key)
-        const isBackspaceClick = evt.key === 'Backspace'
-        const isTabClick = evt.key === 'Tab'
-
-        if (isBackspaceClick ||  isTabClick)
-            return
-
-        if (!parseNumberValue)
-            evt.preventDefault()
+        const inputValue = evt.currentTarget.value.match(/\d/g)
+        if (inputValue) {
+            setPhone(inputValue.join(''))
+        }
+        else {
+            setPhone('')
+        }
     }
 
     const handleSubmit:FormEventHandler = (evt) => {
@@ -35,7 +29,7 @@ export const AddNewChatForm:FC<AddNewChatFormProps> = ({error, onSubmit}) => {
     return (
         <form className={styles.addBewChatForm} onSubmit={handleSubmit}>
             <p className={styles.addBewChatForm__title}>Форма добавления чата</p>
-            <Input extraClass={styles.addBewChatForm__input} value={phone} onKeyDown={handlePhoneInputKeyPress} onChange={handlePhoneChange} placeholder={'Введите номер телефона'} type={"tel"} name={"phone"} required/>
+            <Input extraClass={styles.addBewChatForm__input} value={phone} onChange={handlePhoneChange} placeholder={'Введите номер телефона'} type={"tel"} name={"phone"} required/>
             {error && <p className={styles.addBewChatForm__errorMsg}>{error}</p>}
             <SubmitButton className={styles.addBewChatForm__submitBtn}/>
         </form>
